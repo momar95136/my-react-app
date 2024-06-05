c31a50bf05bedc3a312b05a0a44e99b8a39288fd
koalameedo <meo2@njit.edu>
2024-06-05 12:33:22 +0100

    Using lists & props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import pokeman from "./pokeman.json";
+import PropTypes from "prop-types";
+import { type } from "@testing-library/user-event/dist/type";
+
+const PokemanRow = ({ pokeman }) => (
+  <tr>
+    <td>{pokeman.name.english}</td>
+    <td>{pokeman.type.join(", ")}</td>
+  </tr>
+);
+
+PokemanRow.propTypes = {
+  pokeman: PropTypes.shape({
+    name: PropTypes.shape({ english: PropTypes.string }),
+    type: PropTypes.arrayOf(PropTypes.string),
+  }),
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ margin: "auto", width: 800, paddingTop: "1rem" }}>
+      <h1 className="title">Pokeman Search</h1>
+      <table width="100%">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Type</th>
+          </tr>
+        </thead>
+        <tbody>
+          {pokeman.slice(0, 25).map((pokeman) => (
+            <PokemanRow pokeman={pokeman} key={pokeman.id} />
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
